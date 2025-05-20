662683da9c783c0305d3c94059a126917f86df34
AmmauriNirwan <ammauri8h@gmail.com>
2025-05-20 08:42:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("🎈 Mauri Nirwan ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st. image("IMG-20250518-WA0011.jpg", width= 200) 
+st.image("IMG-20250518-WA0011.jpg", width=200) 
