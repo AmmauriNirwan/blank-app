b4900b251a0d7806ef2861b37f5e1036569b5958
AmmauriNirwan <ammauri8h@gmail.com>
2025-05-27 07:57:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.image("IMG-20250518-WA0011.jpg", width=200)
 
 st.title("Aplikasi Sederhana") 
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil") 
-angka = st.number_input("Tulis sebuah Angka:", value=0, steps=1)
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
 if (angka % 2) == 0: 
     st.write(f"{angka} adalah Bilangan Genap") 
