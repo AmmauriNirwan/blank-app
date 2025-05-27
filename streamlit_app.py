6c87c870d8a0ae41a302414f118e408f9856eef2
AmmauriNirwan <ammauri8h@gmail.com>
2025-05-27 07:51:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,14 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.image("IMG-20250518-WA0011.jpg", width=200) 
+
+
+
+st.title("Aplikasi Sederhana") 
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil") 
+angka = st.number_input("Tulis sebuah Angka:", value=0, steps=1)
+
+if (angka % 2) == 0: 
+    st. writer(f"{angka} adalah Bilangan Genap") 
+else: 
+    st. writer(f"{angka} adalah Bilangan Ganjil") 
